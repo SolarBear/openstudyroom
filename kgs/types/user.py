5f45d7cfd2088dd7461830dbf2943238c99f206f
SolarBear <solarbear@gmail.com>
2017-04-09 09:28:37 -0400

    Moved connection and message code to a dedicated folder. A few new types.

--- kgs/types/user.py
+++ kgs/types/user.py
@@ -4,24 +4,24 @@ class User:
     """
 
     FLAGS = (
-        'g',
-        'c',
-        'd',
-        's',
-        'a',
-        'r',
-        'TT',
-        't',
-        'p',
-        'P',
-        '*',
-        '!',
-        '=',
-        '~',
+        'g',  # guest
+        'c',  # connected
+        'd',  # account deleted
+        's',  # sleeping (inactive for over 10 min)
+        'a',  # user has an avatar
+        'r',  # robot
+        'TT',  # has won a KGS tournament (congrats, buddy!)
+        't',  # runner-up in a KGS tournament (not bad, not bad)
+        'p',  # currently playing a game
+        'P',  # currently playing in a KGS tournament game
+        '*',  # KGS Plus subscriber
+        '!',  # KGS Meijin
+        '=',  # Can play ranked games
+        '~',  # Plays stronger players far more often that weaker ones
     )
 
     AUTH_LEVELS = (
-        'normal', # default
+        'normal',  # default
         'robot_ranked',
         'teacher',
         'jr_admin',
@@ -66,3 +66,17 @@ class User:
             self._auth_level = auth_level
         else:
             raise ValueError('Invalid auth level ' + auth_level)
+
+
+class Friend(User):
+    FRIEND_TYPES = (
+        'buddy',
+        'censored',
+        'fan',
+        'admin_track',
+    )
+
+    def __init__(self, friend_type, name, flags, rank='', auth_level='normal', notes=''):
+        super(User).__init__(name, flags, rank, auth_level)
+        self.friend_type = friend_type
+        self.notes = notes
